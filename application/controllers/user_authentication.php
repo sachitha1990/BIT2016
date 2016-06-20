c89030a4d6c62eb0ed3220e41d2e5aa23be37795
Sachitha <sachitha@openarc.lk>
2016-06-21 02:03:49 +0530

    creating login user authentication controller

--- application/controllers/user_authentication.php
+++ application/controllers/user_authentication.php
@@ -70,5 +70,55 @@ class User_Authentication extends CI_Controller{
         }
 
         //Check for user login process
+        public function user_login_process(){
+
+        $this->form_validation->set_rules('username','Username','trim|required|xss_clean');
+        $this->form_validation->set_rules('password','Password','trim|required|xss_clean');
+
+        if ($this->form_validation->run()== FALSE){
+            if(isset($this->session->userdata['logged_in'])){
+                $this->load->view('admin_page');
+            }else{
+                $this->load->view('login_form');
+            }
+        }else{
+            $data = array(
+                'username' => $this->input->post('username'),
+                'password' => $this->input->post('password')
+            );
+            $result = $this->login_database->login($data);
+            if($result==TRUE){
+                $username = $this->input->post('username');
+                $result = $this->login_database->read_user_information($username);
+                if($result != false){
+                    $session_data = array(
+                        'username' => $result[0]->user_name,
+                        'email'=> $result[0]->user_email,
+                    );
+
+                    // Add user data in session
+                    $this->session->set_userdata('logged_in', $session_data);
+                    $this->load->view('admin_page');
+                }
+            }else{
+                $data = array(
+                    'error_message' => 'Invalid Username or Password'
+                );
+                $this->load->view('login_form',$data);
+            }
+        }
+
+        //Logout from admin page
+        public function logout(){
+
+            //Removing session data
+            $sess_array = array(
+                'username' => ''
+            );
+            $this->session->unset_userdata('logged_in', $sess_array);
+            $data['message_display'] = 'Successfully Logout';
+            $this->load->view('login_form', $data);
+        }
+        }
     }
 }
